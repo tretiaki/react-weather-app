8fb001eee165262d1f07dd052b324aec0a036f1b
tretiaki <tretiak.ira1207@gmail.com>
2022-10-03 18:35:33 +0300

    added API integration🥎

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <Weather />
+      <Weather city="New York" />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,121 +1,146 @@
-import React from "react";
-///import axios from "axios";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div class="container">
-      <div class="weather-app">
-        <form>
+export default function Weather(props) {
+  const [weather, setWeather] = useState({ ready: false });
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeather({
+      ready: true,
+      temperature: Math.round(response.data.main.temp),
+      city: response.data.name,
+      date: "Monday 15:38",
+      humidity: response.data.main.humidity,
+      feelsLike: Math.round(response.data.main.feels_like),
+      description: response.data.weather[0].main,
+      wind: response.data.wind.speed,
+      sunrise: response.data.sys.sunrise,
+      sunset: response.data.sys.sunset,
+    });
+  }
+
+  if (weather.ready) {
+    return (
+      <div class="container">
+        <div class="weather-app">
+          <form>
+            <div class="row">
+              <div class="col-7">
+                <input
+                  type="text"
+                  id="search"
+                  autocomplete="off"
+                  placeholder="Choose a city..."
+                  class="form-control"
+                />
+              </div>
+              <div class="col-1">
+                <input
+                  type="submit"
+                  value="Search🔎"
+                  class="btn buttonSearch"
+                />
+              </div>
+              <div class="col-1">
+                <button id="button" class="btn buttonLocation">
+                  Location📍
+                </button>
+              </div>
+            </div>
+          </form>
           <div class="row">
-            <div class="col-7">
-              <input
-                type="text"
-                id="search"
-                autocomplete="off"
-                placeholder="Choose a city..."
-                class="form-control"
-              />
+            <div class="row maincard">
+              <div class="col column">
+                <img
+                  src="https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/clear-day.svg"
+                  alt="Weather Icon"
+                  class="src icon"
+                  id="icon"
+                />
+              </div>
+              <div class="col column">
+                <div class="card-body">
+                  <h1>
+                    <span class="city">{weather.city}</span>
+                    <br />
+                    <span class="currentDegree" id="currentDegree">
+                      {weather.temperature}
+                    </span>
+                    <span class="temperature">°C</span>
+                  </h1>
+                  <h2>
+                    Last updated:
+                    <span class="currentDay" id="currentTime">
+                      {weather.date}
+                    </span>
+                  </h2>
+                </div>
+              </div>
             </div>
-            <div class="col-1">
-              <input type="submit" value="Search🔎" class="btn buttonSearch" />
+
+            <div class="col column">
+              <ul>
+                <li>
+                  Humidity: <span id="humidity">{weather.humidity}%</span>
+                </li>
+                <li>
+                  Feels like <span id="feelsLike">{weather.feelsLike}°C</span>
+                </li>
+                <li>
+                  <span id="weatherdescription">{weather.description}</span>
+                </li>
+              </ul>
             </div>
-            <div class="col-1">
-              <button id="button" class="btn buttonLocation">
-                Location📍
-              </button>
+            <div class="col column">
+              <ul>
+                <li>
+                  Wind: <span id="wind">{weather.wind} km/h</span>
+                </li>
+                <li>
+                  Sunrise: <span id="sunrise">{weather.sunrise}</span>
+                </li>
+                <li>
+                  Sunset: <span id="sunset">{weather.sunset}</span>
+                </li>
+              </ul>
             </div>
           </div>
-        </form>
-        <div class="row">
-          <div class="row maincard">
-            <div class="col column">
-              <img
-                src="https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/clear-day.svg"
-                alt="Weather Icon"
-                class="src icon"
-                id="icon"
-              />
-            </div>
-            <div class="col column">
+          <div class="row" id="forecast">
+            <div class="card">
               <div class="card-body">
-                <h1>
-                  <span class="city">Kyiv</span>
-                  <br />
-                  <span class="currentDegree" id="currentDegree">
-                    19
-                  </span>
-                  <span class="temperature">°C</span>
-                </h1>
-
-                <h2>
-                  Last updated:
-                  <span class="currentDay" id="currentTime">
-                    Monday 15:38
-                  </span>
-                </h2>
+                <h5 class="card-title weather-forecast-day">Tuesday</h5>
+                <img
+                  src="https://www.amcharts.com/wp-content/themes/amcharts4/css/img/icons/weather/animated/cloudy-day-1.svg"
+                  alt="Partly Cloudy"
+                  class="src emojis"
+                />
+                <p class="card-text">
+                  <span class="temperature-min">13°</span>
+                  <span class="temperature-max">24°</span>
+                </p>
               </div>
             </div>
           </div>
-
-          <div class="col column">
-            <ul>
-              <li>
-                Humidity:<span id="humidity"></span>
-              </li>
-              <li>
-                Feels like <span id="feelsLike"></span>
-              </li>
-              <li>
-                <span id="weatherdescription"></span>
-              </li>
-            </ul>
-          </div>
-          <div class="col column">
-            <ul>
-              <li>
-                Wind:<span id="wind"></span>
-              </li>
-              <li>
-                Sunrise:<span id="sunrise"></span>
-              </li>
-              <li>
-                Sunset:<span id="sunset"></span>
-              </li>
-            </ul>
-          </div>
+          <p>
+            <a href="https://github.com/tretiaki/react-weather-app">
+              Open-source code
+            </a>
+            by Iryna Tretiak👩‍💻
+            <br />
+            Here you find
+            <a href="https://bas.dev/work/meteocons">Animated Weather Icons</a>I
+            used📝
+          </p>
         </div>
-        <div class="row" id="forecast">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title weather-forecast-day">Tuesday</h5>
-              <img
-                src="https://www.amcharts.com/wp-content/themes/amcharts4/css/img/icons/weather/animated/cloudy-day-1.svg"
-                alt="Partly Cloudy"
-                class="src emojis"
-              />
-              <p class="card-text">
-                <span class="temperature-min">13°</span>
-                <span class="temperature-max">24°</span>
-              </p>
-            </div>
-          </div>
-        </div>
-        <p>
-          <a href="https://github.com/tretiaki/react-weather-app">
-            Open-source code
-          </a>
-          by Iryna Tretiak👩‍💻
-          <br />
-          Here you find
-          <a href="https://bas.dev/work/meteocons">Animated Weather Icons</a>I
-          used📝
-        </p>
       </div>
-    </div>
-  );
-}
+    );
+  } else {
+    const apiKey = "d7f80c0687e058642edfcc5becefbc2b";
 
-///let apiKey = "d7f80c0687e058642edfcc5becefbc2b";
-///let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-/// axios.get(apiUrl).then(handleResponse);
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  return <h1>Loading...</h1>;
+}
